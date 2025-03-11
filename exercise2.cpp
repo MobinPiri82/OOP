7d342871a2ba746e0941f8943820af4999c65edd
Mobin Piri <160016623+MobinPiri82@users.noreply.github.com>
2025-03-11 09:37:46 +0330

    Add files via upload

---
+++ exercise2.cpp
@@ -0,0 +1,64 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+class Person
+    {
+        protected :
+            string Name;
+            int Age;
+        public :
+            string GetName()
+                return Name;
+            int GetAge()
+                return Age;
+    }
+    
+    class Student : private Person
+    {
+        private : 
+            double Avg;
+        public :
+            double GetAvg()
+                 return Avg;
+            string GetStudentName()
+                return Name;
+    }          
+    
+    class Teacher : public Person
+    {
+        private :
+            double Salary;
+        public :
+            double GetSalary()
+                return Salary:
+    }
+int main()
+{  
+    Person p; 
+    p.GetName(); //correct
+    p.GetAge(); //correct
+    p.Name; //incorrect
+    p.Age; //incorrect
+    
+    Student s; 
+    s.GetName(); //correct
+    s.GetAge(); //correct
+    s.Avg; //incorrect
+    s.GetAvg(); //correct
+    s.GetStudentName(); //correct
+    
+    Teacher t;
+    t.GetName(); //correct
+    t.GetAge(); //correct
+    t.GetSalary(); //correct
+    t.Salary; //incorrect
+    
+    
+    
+}
