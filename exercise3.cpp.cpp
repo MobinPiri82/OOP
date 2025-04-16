61c0d448850877d31b34749880de9d2e733116a2
Mobin Piri <160016623+MobinPiri82@users.noreply.github.com>
2025-04-16 09:02:26 +0330

    Add files via upload

---
+++ exercise3.cpp.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+
+template<typename T>   
+class parent { 
+protected: 
+    virtual T Add(T a, T b) {  
+        return a + b; 
+    } 
+}; 
+ 
+class child : public parent<int> {  
+public: 
+    int Add(int a, int b) override {  
+         
+        int c = parent<int>::Add(5, 10); 
+ 
+        return c + 20; 
+    } 
+
+int main()
+{
+     child obj; 
+ 
+    cout << obj.Add(1, 2) << endl; 
+}
